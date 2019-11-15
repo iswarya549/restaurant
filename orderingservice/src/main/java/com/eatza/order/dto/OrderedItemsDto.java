11db81ee76e3627dfdc011f531513a16bf53c4dc
M1054472 <M1054472@A2SITML33740.mindtree.com>
2019-11-15 15:55:03 +0530

    pushing

---
+++ orderingservice/src/main/java/com/eatza/order/dto/OrderedItemsDto.java
@@ -0,0 +1,46 @@
+package com.eatza.order.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class OrderedItemsDto {
+	
+	private long itemId;
+	private int quantity;
+	
+
+
+	public OrderedItemsDto(long itemId, int quantity) {
+		super();
+		this.itemId = itemId;
+		this.quantity = quantity;
+	}
+
+
+
+	public long getItemId() {
+		return itemId;
+	}
+
+
+
+	public void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	
+}
